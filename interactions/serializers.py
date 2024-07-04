c427d1565553e63c954feead7a9b4f76f53079eb
Mikhail Kollinz <mikhail.kollinz@motopp.nl>
2024-07-04 17:09:53 +0200

    Extended serializer to retreave users, leads and cintacts information as well

--- interactions/serializers.py
+++ interactions/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
+
+from common.serializer import ProfileSerializer, UserSerializer
+from contacts.serializer import ContactSerializer
+from leads.serializer import LeadSerializer
 from .models import Interaction
 
 class InteractionSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    interact_with = LeadSerializer(read_only=True)
+    contact = ContactSerializer(read_only=True)
+
     class Meta:
         model = Interaction
-        fields = '__all__'
+        fields = ['id', 'user', 'created_at', 'updated_at', 'start_at', 'end_at', 'type', 'interact_with', 'contact', 'description']
