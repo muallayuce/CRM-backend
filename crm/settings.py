c60c5ac3ca69c40383951ca038c917ebe65c29ee
muallayuce <muallayuce@gmail.com>
2024-10-21 20:17:30 +0200

    deleted api key

--- crm/settings.py
+++ crm/settings.py
@@ -168,14 +168,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-#EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
-#MAILGUN_API_KEY = '60e116f26932371b3d730298c63a4612-6fafb9bf-5ce97bdd'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-#EMAIL_USE_SSL = True
+# EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
 DEFAULT_FROM_EMAIL = os.environ["DEFAULT_FROM_EMAIL"]
 EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
@@ -201,8 +200,8 @@ ADMIN_EMAIL = os.environ["ADMIN_EMAIL"]
 # celery Tasks
 CELERY_BROKER_URL = os.environ["CELERY_BROKER_URL"]
 CELERY_RESULT_BACKEND = os.environ["CELERY_RESULT_BACKEND"]
-#CELERY_BROKER_URL = 'redis://localhost:6379'  # Adjust port if needed
-#CELERY_RESULT_BACKEND = 'redis://localhost:6379'  # Adjust port if needed
+# CELERY_BROKER_URL = 'redis://localhost:6379'  # Adjust port if needed
+# CELERY_RESULT_BACKEND = 'redis://localhost:6379'  # Adjust port if needed
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
